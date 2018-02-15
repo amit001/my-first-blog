6be347f48580f572d20a77b962f1d7905f8f9adc
amit <amit.6192@gmail.com>
2018-02-15 20:21:39 +0530

    change in template to display post

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
 	pass
-	return render(request, 'blog/post_list.html')
+	posts = Post.objects.filter(pulished_date__lte=timezone.now()).order_by('pulished_date')
+	return render(request, 'blog/post_list.html',{'posts':posts})
